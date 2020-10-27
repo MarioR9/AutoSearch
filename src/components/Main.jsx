ebe438212d59f3501219d0ab1af95eec8c4db696
Mario Rodriguez <31521338+MarioR9@users.noreply.github.com>
2020-10-26 23:48:00 -0400

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 let carCollection = '';
 let data = []; //collects car divs
-
+let dataTest = []; //test
 /* ? auyo search and connec to craigsts for price check?*/
 
 (async () => {
